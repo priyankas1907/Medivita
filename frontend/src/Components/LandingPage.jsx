de58b602c196ed8de27cfa18802b3f249896e232
priyankas1907 <priyankasalethmary@gmail.com>
2025-02-25 20:18:48 +0530

    changes made

--- frontend/src/Components/LandingPage.jsx
+++ frontend/src/Components/LandingPage.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import './Styles/LandingPage.css';
+import logo from '../Images/logo.png';
 
 const LandingPage = () => {
     return(
-        <div>Hello</div>
+        <div className="landing-page">
+            <br />
+            <div className='navbar'>
+                <img src={logo} alt='logo' className='logo-img'/>
+                <p className='web-name'>Medivita</p>
+                <div>
+                    <button className='sign-in'>Sign in</button>
+                    <button className='sign-up'>Sign up</button>
+                    <button className='about-us'>About Us</button>
+                    <button className='contact-us'>Contact Us</button>
+                </div>
+            </div>
+        </div>
     );
 };
 
