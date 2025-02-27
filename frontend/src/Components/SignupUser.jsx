f1416bdc23d02f95190725b56972a7a28cadc332
priyankas1907 <priyankasalethmary@gmail.com>
2025-02-27 11:14:18 +0530

    Signup for user, doctor, medicine seller Finished

--- frontend/src/Components/SignupUser.jsx
+++ frontend/src/Components/SignupUser.jsx
@@ -1,13 +1,13 @@
 import React, {useState} from 'react';
 import './Styles/SignupUser.css';
-import Navbar from './Navbar.jsx';
+import Navbar3 from './Navbar3.jsx';
 import userImg from '../Images/userImg.png';
 
 const SignupUser = () => {
     return(
         <div>
             {/* Navbar */}
-            <Navbar />
+            <Navbar3 />
 
             {/* body */}
             <div className='grid-container'>
@@ -16,11 +16,11 @@ const SignupUser = () => {
                     <p>Your Name</p>
                     <input type='text' placeholder='Enter your name' />
                     <p>Your Phone No.</p>
-                    <input type='number' placeholder='Enter your phone number' />
+                    <input type='tel' placeholder='Enter your phone number' />
                     <p>Password</p>
-                    <input type='text' placeholder='Enter strong password' />
+                    <input type='password' placeholder='Enter strong password' />
                     <p>Confirm Password</p>
-                    <input type='text' placeholder='Enter your password again' /><br /><br />
+                    <input type='password' placeholder='Enter your password again' /><br /><br />
                     <center><button className='signup-btn'>Sign Up</button></center>
                 </div>
                 <div>
