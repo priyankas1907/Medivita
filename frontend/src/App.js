58d092c34996a8a52c07122fc180d8601fad4c8c
pricillaruby22 <pricillaruby40@gmail.com>
2025-03-08 10:06:34 +0530

    feat:Home page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 //import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './Components/LandingPage.jsx';
+//import LandingPage from './Components/LandingPage.jsx';
+//import Contactus from './Components/Contactus.jsx';
+import HomePage from './Components/HomePage.jsx';
+//import ForgotPassword from './Components/ForgotPassword.jsx';
 
 function App() {
-  return (
+  return ( 
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/forgetpassword" element={<ForgotPassword />} />
+        <Route path="/contactus" element={<Contactus />}/> */}
       </Routes>
     </Router>
   );
